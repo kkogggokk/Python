91604f9a5318269d1dae1b58238dc4be9eb9ccbb
kkogggokk <kkogggokk@gmail.com>
2021-03-02 23:46:04 +0900

    update

---
+++ 02_Algorithm/02_Test/01_BaekJoon/01_입출력과사칙연산/10430_나머지.py
@@ -0,0 +1,3 @@
+# URL : https://www.acmicpc.net/problem/10430
+A, B, C = map(int,input().split())
+print((A+B)%C, ((A%C) + (B%C))%C, (A*B)%C, ((A%C)*(B%C))%C, sep = '\n')
---
+++ 02_Algorithm/02_Test/01_BaekJoon/01_입출력과사칙연산/2588_곱셈.py
@@ -0,0 +1,5 @@
+# URL : https://www.acmicpc.net/problem/2588
+
+num1, num2 = map(int,input().split())
+
+list(num2)
