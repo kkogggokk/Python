bfc92f50b5b6abf27dae47e396dcc21b6fe77684
kkogggokk <kkogggokk@gmail.com>
2021-03-06 20:44:14 +0900

    update

--- 02_Algorithm/02_Test/01_BaekJoon/01_입출력과사칙연산/2588_곱셈.py
+++ 02_Algorithm/02_Test/01_BaekJoon/01_입출력과사칙연산/2588_곱셈.py
@@ -1,5 +1,10 @@
 # URL : https://www.acmicpc.net/problem/2588
 
-num1, num2 = map(int,input().split())
+num1 = int(input())
+num2 = int(input())
 
-list(num2)
+num2_lst = [i for i in str(num2)] # seperate the digits of each number
+
+for i in range(2,-1, -1) : # call in reverse to calculate from low digits
+    print(num1 * int(num2_lst[i])) # convert characters to numbers for computational operations
+print(num1 * num2)
